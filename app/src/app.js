e122649102df7043bedab166aa2fcfaa4a55d604
Guilherme Carvalho <guilhermercarvalho512@gmail.com>
2020-10-31 03:32:18 +0000

    add body-parser

--- app/src/app.js
+++ app/src/app.js
@@ -1,29 +1,33 @@
-import express from "express";
-const app = express();
+import express from 'express'
+import bodyParser from 'body-parser'
 
-import router from './routes';
+import router from './routes'
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res, next) => {
-    return res.redirect('/alunos')
+  return res.redirect('/alunos')
 })
 
 app.use('/alunos', router)
 
 app.use((req, res, next) => {
-    res.status(404).send({
-        erro: {
-            mensagem: "Endpoind não encontrado"
-        }
-    })
-});
+  res.status(404).send({
+    erro: {
+      mensagem: 'Endpoint não encontrado'
+    }
+  })
+})
 
 app.use((error, req, res, next) => {
-    res.status([error.status] || 500);
-    return res.send({
-        erro: {
-            mensagem: error.message
-        }
-    });
-});
+  res.status([error.status] || 500)
+  return res.send({
+    erro: {
+      mensagem: error.message
+    }
+  })
+})
 
-export default app;
+export default app
