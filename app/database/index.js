17bffcc2e8fa1af00a1d82d923aab7ca5631c7ca
Guilherme Carvalho <dev.carvalho@protonmail.com>
2022-04-02 04:24:04 +0000

    refactor: delete previus project

--- app/database/index.js
+++
@@ -1,7 +0,0 @@
-import { Sequelize } from 'sequelize'
-import path from 'path'
-
-export default new Sequelize({
-  dialect: 'sqlite',
-  storage: path.join(__dirname, '/aluno.sqlite')
-})
--- app/index.js
+++
@@ -1,4 +0,0 @@
-import app from './src/app'
-
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Listening: http://localhost:${PORT}`))
