00b042fb9b3a0b62c0f02f441139ec68364359e7
Guilherme Carvalho <dev.carvalho@protonmail.com>
2022-08-19 02:23:14 +0000

    refactor: add redirection and not found error

--- src/main/frameworks/express/setup-routes.ts
+++ src/main/frameworks/express/setup-routes.ts
@@ -1,9 +1,22 @@
-import { Express } from 'express';
+import { Express, Request, Response } from 'express';
 import { Database } from 'infra/contracts/database';
 import apiVersion from 'main/routes/express/v1';
 import routes from 'main/config/routes';
 
 export default (app: Express, dataSource: Database): void => {
   const apiMiddleWare = apiVersion(dataSource);
+
   app.use(routes.version, apiMiddleWare);
+
+  app.get('/', (_req, res) => {
+    return res.redirect(`${routes.version}${routes.students}`);
+  });
+
+  app.use((_req, res) => {
+    res.status(404).json({
+      data: {
+        error: 'Endpoint not found'
+      }
+    });
+  });
 };
