a66fa7d2a7496ce0b72e17075e500c031db815fd
Guilherme Carvalho <dev.carvalho@protonmail.com>
2022-09-25 14:20:27 -0400

    feat (express): include query in request

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -6,7 +6,8 @@ export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const request = {
       ...(req.body || {}),
-      ...(req.params || {})
+      ...(req.params || {}),
+      ...(req.query || {})
     };
     const httpResponse = await controller.handle(request);
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
