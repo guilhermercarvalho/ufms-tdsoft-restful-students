9f6926d77cbb5d7b90e09831c2ff6d18cfcb0731
Guilherme Carvalho <dev.carvalho@protonmail.com>
2022-09-25 14:19:42 -0400

    refactor (errors): refactor some errors

--- src/presentation/errors/invalid-param-type-error.ts
+++ src/presentation/errors/invalid-param-type-error.ts
@@ -1,6 +1,8 @@
 export class InvalidParamTypeError extends Error {
   constructor(paramName: string, typeExpected: string) {
-    super(`Invalid param type: ${paramName} expected type was ${typeExpected}`);
+    super(
+      `Invalid parameter type '${paramName}' expected was '${typeExpected}'`
+    );
     this.name = 'InvalidParamTypeError';
   }
 }
